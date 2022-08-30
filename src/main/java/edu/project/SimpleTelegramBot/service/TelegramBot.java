8b790023cb9bc113e64aff376138e964617ace99
Andera Bridger <abridger@vo-i5.msk.21-school.ru>
2022-08-30 18:39:17 +0300

    classes telegrambot, botinitializer

--- src/main/java/edu/project/SimpleTelegramBot/service/TelegramBot.java
+++ src/main/java/edu/project/SimpleTelegramBot/service/TelegramBot.java
@@ -1,4 +1,68 @@
 package edu.project.SimpleTelegramBot.service;
 
-public class TelegramBot {
+import edu.project.SimpleTelegramBot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatID = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatID, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatID, "Sorry, command was not recognized!");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatID, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
+        log.info("Replied to user " + name);
+        sendMessage(chatID, answer);
+    }
+
+    private void sendMessage(long chatID, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatID));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        }
+        catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+
+    }
+
+
 }
